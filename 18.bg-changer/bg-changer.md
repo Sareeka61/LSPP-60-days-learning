7a9d4e1f6a13bd91ce3734e4dcf97ec0ca9e02ce
sareeka61 <ghimiresarika61@gmail.com>
2023-06-19 00:11:50 +0545

    LSPP Day 17 Completed

---
+++ 18.bg-changer/bg-changer.md
@@ -0,0 +1,3 @@
+# Background changer using JS
+
+Today I made a very simple background changer logic using JS. 
---
+++ 18.bg-changer/script.js
@@ -0,0 +1,24 @@
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const btn4 = document.querySelector("#btn4");
+
+btn1.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    "url('https://i.pinimg.com/originals/39/25/e3/3925e31cdd0b8f583cab882a39c88fc2.gif')";
+});
+
+btn2.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    "url('https://i.pinimg.com/736x/43/ed/50/43ed50f03e9a2b4054f5d6833bd17d97.jpg')";
+});
+
+btn3.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    "url('https://thumbs.gfycat.com/AdorableAshamedHoki-max-1mb.gif')";
+});
+
+btn4.addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    "url('https://usagif.com/wp-content/uploads/2022/hqgif/nezuko-acegif-41.gif')";
+});
